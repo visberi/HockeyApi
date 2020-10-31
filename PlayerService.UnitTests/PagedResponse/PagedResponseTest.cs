862bdbd2330973560d6d2eadae2041b983577e97
Visa <visberi@hotmail.com>
2020-10-31 21:53:13 +0200

    Add unit tests for pagination components

---
+++ PlayerService.UnitTests/PagedResponse/PagedResponseTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayerService.Contracts;
+using PlayerService.Controllers;
+using Xunit;
+
+namespace PlayerService.UnitTests
+{
+    public  class PagedResponseTest
+    {
+        private List<int> data;
+        private int defaultDataCount = 25;
+
+
+        private PaginationParameters paginationParameters;
+        public PagedResponseTest()
+        {
+            data = new List<int>();
+            for (int i = 0; i < defaultDataCount; i++)
+            {
+                data.Add(i);
+            }
+
+            // Create default pagination parameters
+            paginationParameters = new PaginationParameters();
+        }
+
+        [Fact]
+        private void DefaultDataThirdPage_HasFiveItems()
+        {
+            paginationParameters = new PaginationParameters(10,3);
+            PagedResponse<int> paginatedData = new PagedResponse<int>(data,paginationParameters);
+            Assert.Equal(5,paginatedData.Data.Count());
+        }
+
+        [Fact]
+        private void DefaultDataFirstPageLast_EqualsToNine()
+        {
+            PagedResponse<int> paginatedData = new PagedResponse<int>(data,paginationParameters);
+            Assert.Equal(9, paginatedData.Data.ToList()[9]);
+        }
+
+        [Fact]
+        private void EmptyData_ReturnsEmpty()
+        {
+            data.Clear();
+            PagedResponse<int> paginatedData = new PagedResponse<int>(data,paginationParameters);
+            Assert.Empty( paginatedData.Data);
+        }
+    }
+}
---
+++ PlayerService.UnitTests/PagedResponse/PaginationParametersTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlayerService.Controllers;
+using Xunit;
+
+namespace PlayerService.UnitTests
+{
+    public class PaginationParametersTest
+    {
+        [Fact]
+        private void NewParamsPageSizeOverMax_SetToMaxSize()
+        {
+            var paginationParameters = new PaginationParameters(856, 2);
+            Assert.Equal(PaginationParameters.MaxPageSize, paginationParameters.PageSize);
+        }
+
+        [Fact]
+        private void NewParamsPageSizeZero_SetToMinimumSize()
+        {
+            var paginationParameters = new PaginationParameters(0, 2);
+            Assert.Equal(PaginationParameters.MinPageSize, paginationParameters.PageSize);
+        }
+    }
+}
